9a2e902e60fb7f8897b880c8fd11d5319ed5e93b
Maksym Demianenko <106101946+Maxdnua@users.noreply.github.com>
2023-05-15 09:58:40 +0200

    Update path SKU file

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-top_novart = pd.read_csv('C:/Users/Work Runa Art/Downloads/Novart_top.csv',sep=';')
+top_novart = pd.read_csv('Novart_top.csv',sep=';')
 
 ean = st.text_input('Scan Image', value="")[:8]
 if ean in top_novart['SKU'].tolist():
